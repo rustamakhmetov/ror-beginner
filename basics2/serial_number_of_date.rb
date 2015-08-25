d870f8f9bb1792c44cbb9c1aeb4ede7d34b16f4f
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-08-25 23:53:05 +0500

    Fixed serial number of date

--- basics2/serial_number_of_date.rb
+++ basics2/serial_number_of_date.rb
@@ -1,16 +1,21 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 
-require 'date'
-
 # Заданы три числа, которые обозначают число, месяц, год (запрашиваем у пользователя).
 # Найти порядковый номер даты, начиная отсчет с начала года. Учесть, что год может быть високосным
 
-puts "Enter day:"
+puts "Введите день:"
 day = gets.to_i
-puts "Enter month:"
+puts "Введите месяц:"
 month = gets.to_i
-puts "Enter year:"
+puts "Введите год:"
 year = gets.to_i
 
-puts "Number of the day: #{Date.new(y=year,m=month,d=day).yday}"
+months = [nil,31,28,31,30,31,30,31,31,30,31,30,31]
+months[2] = 29 if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+if day>months[month]
+  puts "Неверная дата. В этом месяце #{months[month]} дней"
+  exit
+end
+day_number = (month>1 ? months.slice(1, month-1).inject(:+) : 0) + day
+puts "Порядковый номер даты: #{day_number}"
