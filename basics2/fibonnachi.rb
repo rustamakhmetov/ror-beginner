20df8221ff0095f2f5922d70447f859349388abc
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-08-26 09:12:00 +0500

    Fixed fibonnachi

--- basics2/fibonnachi.rb
+++ basics2/fibonnachi.rb
@@ -3,10 +3,21 @@
 
 # Заполнить массив числами фибоначи до 100
 def fib(n)
-  (@d ||= [1,1])[n] ||= (
+  (@d ||= [0,1,1])[n] ||= (
   fib(n-1) + fib(n-2)
   )
 end
 
-fib(100)
-puts @d
+def fib2(n)
+  f = [0, 1]
+  (2..n).each do |i|
+    f << (f[i-1] + f[i-2])
+  end
+  f
+end
+
+number = 100
+
+#fib(number)
+#p @d
+puts fib2(number)
