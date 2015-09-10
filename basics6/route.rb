f5bc03ccb6cbf6abdec340b6fbc3d1e8d78cd86c
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-10 10:47:30 +0500

    Initial basics6

---
+++ basics6/route.rb
@@ -0,0 +1,51 @@
+# -*- encoding: utf-8 -*-
+
+#Маршрут:
+# Имеет начальную и конечную станцию, а также список промежуточных станций
+# Может добавлять станцию в список
+# Может удалять станцию из списка
+# Может выводить список всех станций по-порядку от начальной до конечной
+
+require_relative 'railway_station'
+
+class Route
+  attr_reader :stations
+
+  def initialize(stations)
+    @stations = stations
+  end
+  def +(station)
+    @stations << station unless @stations.include?(station)
+    self
+  end
+  def -(station)
+    @stations.delete(station)
+    self
+  end
+  def first
+    @stations.first
+  end
+  def last
+    @stations.last
+  end
+  def next(station)
+    if last!=station
+      @stations[@stations.index(station)+1]
+    else
+      nil
+    end
+  end
+  def prev(station)
+    if first!=station
+      @stations[@stations.index(station)-1]
+    else
+      nil
+    end
+  end
+  def to_s
+    s = []
+    @stations.each_with_index { |station, i| s << "#{i+1}. #{station}"}
+    s.join("\n")
+  end
+
+end
---
+++ basics6/train.rb
@@ -0,0 +1,116 @@
+# -*- encoding: utf-8 -*-
+
+#Поезд:
+# Имеет, тип, который указывается при создании: грузовой, пассажирский и количество вагонов.
+# Поезд может делать следующие вещи:
+#  набирать скорость
+#  показывать текущую скорость
+#  тормозить
+#  показывать количество вагонов
+#  прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов).
+#    Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
+# Принимать маршрут следования
+# Перемещаться между станциями, указанными в маршруте.
+# Показывать предыдущую станцию, текущую, следующую, на основе маршрута
+require_relative 'passanger_wagon'
+require_relative 'cargo_wagon'
+require_relative 'manufacturer'
+require_relative 'instance_counter'
+
+
+class Train
+  CARGO = :cargo
+  PASSANGER = :passanger
+
+  include InstanceCounter
+  include Manufacturer
+
+  attr_accessor :speed
+  attr_reader :wagons, :type, :route, :station
+
+  @@trains = Hash.new
+
+  def initialize(type, number)
+    @type = type
+    @wagons = []
+    @speed = 0
+    @number = number
+    @@trains[number] = self
+    register_instance
+    #(1..count_wagons).each { |i| add_wagon }
+  end
+
+  def self.find(train_id)
+    @@trains[train_id] if @@trains.key?(train_id)
+  end
+
+  def route=(route)
+    @route = route
+    @station = route.first
+    @station << self
+  end
+
+  def station=(station)
+    @station >> self if @station
+    @station = station
+    @station << self
+  end
+
+  def prev_station
+    @route.prev(station)
+  end
+
+  def next_station
+    @route.next(station)
+  end
+
+  def go
+    if !next_station.nil?
+      if @speed == 0
+        @speed = 10
+      end
+      @station >> self # delete train from station
+      @station = next_station
+      @station << self # add train to station
+    else
+      stop
+    end
+    @speed>0
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def add_wagon(wagon)
+    if @speed==0 && wagon.type==@type
+      @wagons << wagon
+    end
+    @wagons.size
+  end
+
+  def del_wagon(wagon)
+    if @speed==0
+      @wagons.delete_at(@wagons.find_index{|x| x.type == wagon.type} || @wagons.size)
+    end
+    @wagons.size
+  end
+
+  def terminal_station?
+    station==route.last
+  end
+
+  def to_s
+    "#{@type.to_s.capitalize} N#{@number} (#{@wagons.size} wag.)"
+  end
+end
+
+if __FILE__ == $0
+  t1 = Train.new(Train::CARGO, 1)
+  t1.manufacturer = "R1"
+  puts t1.manufacturer
+  puts Train.find(1) # output: Cargo N1 (0 wag.)
+  puts Train.find(0) # output; nil
+  t2 = Train.new(Train::CARGO, 2)
+  puts "Train instances: #{Train.instances}"
+end
